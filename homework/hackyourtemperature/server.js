55da1ac79bc8f69c87f6471fbd98806aadfcca1e
yusufelik <ysfelik1@gmail.com>
2023-01-17 19:07:42 +0100

    USE route for express.json

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 
+app.use(express.json());
+
 app.post('/weather', (req, res) => {
   const cityName = req.body.cityName;
   res.send(cityName);
